bdac28d9ce0f1f94dd1c1ed23580c9b1239e345a
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-17 12:38:44 +0100

    populates the flight search dropdowns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.order(:airport_code)
+    @passenger_options = (1..4).to_a
+    @flight_dates = Flight
+      .select("DISTINCT DATE(start) AS flight_date")
+      .order("flight_date")
+      .map { |f| Date.parse(f.flight_date) }
   end
 end
