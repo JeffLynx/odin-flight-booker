d0e6d1650fb211fab77dae42f57702c18ca2da55
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-18 13:22:34 +0100

    ticket booking flow complete

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,33 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
-    passenger_count = params[:passengers].to_i
-
     @booking = Booking.new(flight: @flight)
 
-    passenger_count.times { @booking.passengers.build }
+    params[:passengers].to_i.times do
+      @booking.passengers.build
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Booking successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(
+      :flight_id,
+      passengers_attributes: [ :name, :email ]
+    )
   end
 end
