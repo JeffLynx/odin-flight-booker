f2caf0f0b13d0690e1e68fc59c1f445c11cb0ddb
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-18 13:38:15 +0100

    updates test for BookingsController

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class BookingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get bookings_new_url
+  test "should get new with flight and passengers" do
+    airport1 = Airport.create!(airport_code: "NYC")
+    airport2 = Airport.create!(airport_code: "SFO")
+
+    flight = Flight.create!(
+      departure_airport: airport1,
+      arrival_airport: airport2,
+      start: Time.zone.local(2026, 6, 25, 6, 0, 0),
+      duration: 120
+    )
+
+    get bookings_new_url, params: { flight_id: flight.id, passengers: 2 }
+
     assert_response :success
   end
 end
